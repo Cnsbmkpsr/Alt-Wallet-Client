c6abd3032b0d884f0cd67e05c20d84ada6d8ec02
Cnsbmkpsr <remib33@gmail.com>
2021-11-23 13:55:35 +0100

    get more informations and improve UI

--- alt-wallet-client/src/Dashboard.js
+++ alt-wallet-client/src/Dashboard.js
@@ -4,13 +4,14 @@ import ErrorMessage from "./ErrorMessage";
 import TxList from "./TxList";
 import { resolveProperties } from "@ethersproject/properties";
 
-
+let APIConnexion = false;
 
 
 const testConnexionToApi = async () => {
     console.log("[ ]  Test Connexion to API...")
     return new Promise((resolve, reject) => {
         try {
+            // URL de l'API ici
             var url = "http://localhost:7546/"
             var request = new XMLHttpRequest();
             request.open('GET', url);
@@ -19,8 +20,7 @@ const testConnexionToApi = async () => {
 
             fetch(url).then(function (response) {
                 response.text().then(function (text) {
-                    //console.log("reponse: ");
-                    //console.log(text);
+                    APIConnexion = true;
                     resolve(text);
                 });
             });
@@ -31,29 +31,49 @@ const testConnexionToApi = async () => {
     })
 }
 
-const getWalletInformation = async ({ setApiStatus, apiStatus }) => {
-    console.log("[ ]  Get Wallet Informations...")
+const getWalletInformation = async ({ setApiStatus }) => {
     try {
         setApiStatus(await testConnexionToApi());
         await window.ethereum.send("eth_requestAccounts");
 
         const provider = new ethers.providers.Web3Provider(window.ethereum);
+        let networkName = (await provider.getNetwork()).name;
+
         const signer = provider.getSigner();
+        let signerAddress = (await signer.getAddress());
+
+        let signerWalletTransactionCount = await signer.getTransactionCount();
+
     } catch (err) {
         console.log(err);
     }
 }
 
-
 export default function Dashboard() {
-    const [apiStatus, setApiStatus] = useState("Offline");
-    getWalletInformation({ setApiStatus, apiStatus });
+    const [apiStatus, setApiStatus] = useState();
+    getWalletInformation({ setApiStatus });
+
+    setInterval(getWalletInformation, 10000, { setApiStatus });
 
 
     return (
         <div className="bg-gray-600 text-center text-white">
             <h1>Bienvenue sur votre Dashboard cliente ETH</h1>
-            <h2>API Status: {apiStatus}</h2>
+
+            {/* API Status */}
+            {
+                !apiStatus &&
+                <h1 className="bg-red-600">API Status: Offline</h1>
+            }
+            {
+                apiStatus &&
+                <h1 className="bg-green-600">API Status: Online</h1>
+            }
+
+            {
+                !APIConnexion &&
+                <h1 className="bg-yellow-600">Please wait, we are trying to connect to the API...</h1>
+            }
         </div>
     )
 }
