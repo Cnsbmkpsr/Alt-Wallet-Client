7ca3c53beec3dbec726c11ddeacb5a81a6503313
Cnsbmkpsr <remib33@gmail.com>
2021-11-23 09:51:23 +0100

    UI Check connexion to API

--- alt-wallet-client/src/Dashboard.js
+++ alt-wallet-client/src/Dashboard.js
@@ -2,38 +2,45 @@ import { useState } from "react";
 import { ethers } from "ethers";
 import ErrorMessage from "./ErrorMessage";
 import TxList from "./TxList";
+import { resolveProperties } from "@ethersproject/properties";
 
-var url = "http://localhost:7546/"
-var request = new XMLHttpRequest();
-request.open('GET', url);
-request.responseType = 'text';
-request.send();
 
-fetch(url).then(function (response) {
-    response.text().then(function (text) {
-        console.log("reponse: ");
-        console.log(text);
-    });
-});
 
 
 const testConnexionToApi = async () => {
     console.log("[ ]  Test Connexion to API...")
-    try {
-        await window.ethereum.send("eth_requestAccounts");
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        console.log(provider.network.name);
-        const signer = provider.getSigner();
-        console.log(signer)
-    } catch (err) {
-        console.log(err);
-    }
+    return new Promise((resolve, reject) => {
+        try {
+            var url = "http://localhost:7546/"
+            var request = new XMLHttpRequest();
+            request.open('GET', url);
+            request.responseType = 'text';
+            request.send();
+
+            fetch(url).then(function (response) {
+                response.text().then(function (text) {
+                    //console.log("reponse: ");
+                    //console.log(text);
+                    resolve(text);
+                });
+            });
+
+        } catch (err) {
+            console.log(err);
+        }
+    })
 }
 
-const getWalletInformation = async () => {
+const getWalletInformation = async ({ setApiStatus, apiStatus }) => {
     console.log("[ ]  Get Wallet Informations...")
     try {
-        await testConnexionToApi();
+        console.log(apiStatus);
+        setApiStatus(await testConnexionToApi());
+        await window.ethereum.send("eth_requestAccounts");
+        console.log(apiStatus);
+
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
     } catch (err) {
         console.log(err);
     }
@@ -42,12 +49,12 @@ const getWalletInformation = async () => {
 
 export default function Dashboard() {
     const [apiStatus, setApiStatus] = useState("Not connected");
-    getWalletInformation();
+    getWalletInformation({ setApiStatus, apiStatus });
 
 
     return (
-        <div className="bg-gray-600">
-            <h1>Dashboard here</h1>
+        <div className="bg-gray-600 text-center text-white">
+            <h1>Bienvenue sur votre Dashboard cliente ETH</h1>
             <h2>{apiStatus}</h2>
         </div>
     )
