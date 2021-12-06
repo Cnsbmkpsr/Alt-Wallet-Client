5ea4aee8c1bb49e9009e9035288be0a03cd74031
Cnsbmkpsr <44788831+Cnsbmkpsr@users.noreply.github.com>
2021-12-06 07:58:25 +0100

    Update Dashboard.js

--- alt-wallet-client/src/Dashboard.js
+++ alt-wallet-client/src/Dashboard.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ethers } from "ethers";
 import ErrorMessage from "./ErrorMessage";
 import TxList from "./TxList";
@@ -8,73 +8,77 @@ import { resolveProperties } from "@ethersproject/properties";
 
 let APIConnexion = false;
 
-
-const testConnexionToApi = () => {
-    return new Promise((resolve, reject) => {
-        try {
-            // URL de l'API ici
-            var url = "http://localhost:7546/"
-            var request = new XMLHttpRequest();
-            request.open('GET', url);
-            request.responseType = 'text';
-            request.send();
-
-            fetch(url).then(function (response) {
-                response.text().then(function (text) {
-                    APIConnexion = true;
-                    resolve(text);
-                });
-            });
-
-        } catch (err) {
-            console.log(err);
-            APIConnexion = false;
-            reject(null);
-        }
-    })
-
-}
-
-const getWalletInformation = async ({ setApiStatus, setNetworkName, setSignerAddress, setSignerBalance, setSignerWalletTransactionCount }) => {
-    try {
-        await window.ethereum.send("eth_requestAccounts");
-
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        let networkName = (await provider.getNetwork()).name;
-        setNetworkName(networkName);
-
-        const signer = provider.getSigner();
-        let signerAddress = (await signer.getAddress());
-        setSignerAddress(signerAddress);
-
-        let signerBalance = (await ethers.utils.formatEther(await signer.getBalance()));
-        setSignerBalance(signerBalance);
-
-        let signerWalletTransactionCount = await signer.getTransactionCount();
-        setSignerWalletTransactionCount(signerWalletTransactionCount);
-
-        setApiStatus(await testConnexionToApi());
-
-    } catch (err) {
-        console.log(err);
-    }
-}
-
 export default function Dashboard() {
 
-    const [apiStatus, setApiStatus] = useState();
+    const [apiStatus, setApiStatus] = useState(null);
     const [networkName, setNetworkName] = useState();
     const [signerAddress, setSignerAddress] = useState();
     const [signerBalance, setSignerBalance] = useState();
     const [signerWalletTransactionCount, setSignerWalletTransactionCount] = useState();
-    getWalletInformation({ setApiStatus, setNetworkName, setSignerAddress, setSignerBalance, setSignerWalletTransactionCount });
 
-    setInterval(getWalletInformation, 10000, { setApiStatus, setNetworkName, setSignerAddress, setSignerBalance, setSignerWalletTransactionCount });
+    useEffect(() => {
+        const testConnexionToApi = () => {
+            return new Promise((resolve, reject) => {
+                try {
+                    // URL de l'API ici
+                    var url = "http://localhost:7546/"
+                    var request = new XMLHttpRequest();
+                    request.open('GET', url);
+                    request.responseType = 'text';
+                    request.send();
+
+                    fetch(url).then(function (response) {
+                        response.text().then(function (text) {
+                            APIConnexion = true;
+                            resolve(text);
+                        });
+                    });
+
+                } catch (err) {
+                    console.log(err);
+                    APIConnexion = false;
+                    reject(null);
+                }
+            })
+
+        }
+
+        const getWalletInformation = async ({ setApiStatus, setNetworkName, setSignerAddress, setSignerBalance, setSignerWalletTransactionCount }) => {
+            try {
+                console.log("get wallte information")
+                const provider = new ethers.providers.Web3Provider(window.ethereum);
+                let networkName = (await provider.getNetwork()).name;
+                setNetworkName(networkName);
+
+                const signer = provider.getSigner();
+                let signerAddress = (await signer.getAddress());
+                setSignerAddress(signerAddress);
+
+                let signerBalance = (await ethers.utils.formatEther(await signer.getBalance()));
+                setSignerBalance(signerBalance);
+
+                let signerWalletTransactionCount = await signer.getTransactionCount();
+                setSignerWalletTransactionCount(signerWalletTransactionCount);
+
+                await window.ethereum.send("eth_requestAccounts");
+
+
+                setApiStatus(await testConnexionToApi());
+
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        setInterval(getWalletInformation, 10000, { setApiStatus, setNetworkName, setSignerAddress, setSignerBalance, setSignerWalletTransactionCount });
+
+    });
+
 
 
     return (
         <div className="text-center text-gray-800">
-            <h1>Bienvenue sur votre Dashboard cliente EETH</h1>
+            <h1>Bienvenue sur votre Dashboard cliente ETH</h1>
 
             {/* API Status */}
             {
