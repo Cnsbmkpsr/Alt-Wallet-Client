c74dc32663c378c2937aab41885d6c329d2d3ccc
Cnsbmkpsr <44788831+Cnsbmkpsr@users.noreply.github.com>
2021-12-09 23:49:53 +0100

    get Token information

--- nextjs-alt-wallet-client/pages/customTokenTransaction.js
+++ nextjs-alt-wallet-client/pages/customTokenTransaction.js
@@ -10,6 +10,11 @@ const customTokenTransaction = () => {
 
     const [userAccount, setUserAccount] = useState()
     const [amount, setAmount] = useState()
+    const [tokenAddress, setTokenAddress] = useState();
+    const [tokenName, setTokenName] = useState();
+    const [tokenSupply, setTokenSupply] = useState();
+    const [tokenOwnerAddress, setTokenOwnerAddress] = useState();
+    const [tokenSymbol, setTokenSymbol] = useState();
 
     async function requestAccount() {
         await window.ethereum.request({ method: 'eth_requestAccounts' });
@@ -37,17 +42,48 @@ const customTokenTransaction = () => {
         }
     }
 
+    async function getTokenInformation() {
+        if (typeof window.ethereum !== "undefined") {
+            await requestAccount();
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            const contract = new ethers.Contract(altTokenAddress, Token.abi, signer);
+            const contractAddress = contract.address;
+            const contractFunctions = contract.functions;
+            const contractName = await contract.name();
+            const contractSupply = await contract.totalSupply();
+            const contractOwner = await contract.owner();
+            const contractSymbol = await contract.symbol();
+
+            setTokenAddress(contractAddress);
+            setTokenName(contractName);
+            setTokenSupply(contractSupply.toNumber());
+            setTokenOwnerAddress(contractOwner);
+            setTokenSymbol(contractSymbol);
+
+        }
+    }
+
+    getTokenInformation();
 
     return (
         <div>
             <Navbar />
             <h1>Custom token transaction</h1>
-            <div>
+            <div className="flex flex-col bg-gray-400 p-4 m-4">
                 <button onClick={requestAccount}>Request account</button>
                 <button onClick={getBalance}>Get Balance</button>
                 <button onClick={sendCoins}>Send Coins</button>
+                <button onClick={getTokenInformation}>Get Token Information</button>
                 <input onChange={e => setUserAccount(e.target.value)} placeholder="Account ID" />
                 <input onChange={e => setAmount(e.target.value)} placeholder="Amount" />
+            </div>
+            <div className="bg-blue-300">
+                {tokenAddress ? <h2>Address du Token: {tokenAddress}</h2> : <h2>Chargement...</h2>}
+                {tokenAddress ? <h2>Address du Token: {tokenAddress}</h2> : <h2>Chargement...</h2>}
+                {tokenAddress ? <h2>Address du Token: {tokenAddress}</h2> : <h2>Chargement...</h2>}
+                {tokenAddress ? <h2>Address du Token: {tokenAddress}</h2> : <h2>Chargement...</h2>}
+                {tokenAddress ? <h2>Address du Token: {tokenAddress}</h2> : <h2>Chargement...</h2>}
             </div>
         </div>
     )
