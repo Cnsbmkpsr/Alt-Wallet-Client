68458f42e45bc096755e2bc2c598b72883c3d60b
Cnsbmkpsr <44788831+Cnsbmkpsr@users.noreply.github.com>
2021-12-11 08:45:38 +0100

    Update tailwind.config.js

--- nextjs-alt-wallet-client/tailwind.config.js
+++ nextjs-alt-wallet-client/tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     locales: ["en-US"],
     defaultLocale: "en-US",
   },
-  purge: ['./src/components/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       backgroundImage: (theme) => ({
