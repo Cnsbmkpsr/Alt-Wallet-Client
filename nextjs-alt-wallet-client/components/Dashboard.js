4a4bae2faef95976ea9c6d5dfc55c570546bc81c
Cnsbmkpsr <44788831+Cnsbmkpsr@users.noreply.github.com>
2021-12-16 15:50:19 +0100

    Starting API Dashboard

--- nextjs-alt-wallet-client/components/Dashboard.js
+++ nextjs-alt-wallet-client/components/Dashboard.js
@@ -6,39 +6,12 @@ let APIConnexion = false;
 
 export default function Dashboard() {
 
-    const [apiStatus, setApiStatus] = useState(null);
     const [networkName, setNetworkName] = useState();
     const [signerAddress, setSignerAddress] = useState();
     const [signerBalance, setSignerBalance] = useState();
     const [signerWalletTransactionCount, setSignerWalletTransactionCount] = useState();
 
-    const testConnexionToApi = () => {
-        return new Promise((resolve, reject) => {
-            try {
-                var url = "http://localhost:7546/"
-                var request = new XMLHttpRequest();
-                request.open('GET', url, true);
-                request.timeout = 5000;
-                request.responseType = 'text';
-                request.send();
-                fetch(url).then(function (response) {
-                    response.text().then(function (text) {
-                        APIConnexion = true;
-                        resolve(text);
-                    });
-                });
-                request.ontimeout = function () {
-                    setApiStatus(null);
-                }
-            } catch (err) {
-                throw new Error(err);                    //console.log(err);
-                APIConnexion = false;
-                setApiStatus(null);
-            }
-        })
-    }
-
-    const getWalletInformation = async ({ setApiStatus, setNetworkName, setSignerAddress, setSignerBalance, setSignerWalletTransactionCount }) => {
+    const getWalletInformation = async () => {
         try {
             const provider = new ethers.providers.Web3Provider(window.ethereum);
             let networkName = (await provider.getNetwork()).name;
@@ -57,16 +30,14 @@ export default function Dashboard() {
             await window.ethereum.send("eth_requestAccounts");
 
 
-            setApiStatus(await testConnexionToApi());
-
         } catch (err) {
             console.log(err);
         }
     }
 
     useEffect(() => {
-        setInterval(getWalletInformation, 1000, { setApiStatus, setNetworkName, setSignerAddress, setSignerBalance, setSignerWalletTransactionCount })
-        getWalletInformation({ setApiStatus, setNetworkName, setSignerAddress, setSignerBalance, setSignerWalletTransactionCount });
+        setInterval(getWalletInformation, 1000);
+        getWalletInformation();
 
     }, []);
 
@@ -76,15 +47,6 @@ export default function Dashboard() {
         <div className="text-center text-gray-800">
             <h1>Bienvenue sur votre Dashboard cliente ETH</h1>
 
-            {/* API Status */}
-            {
-                !apiStatus &&
-                <h1 className="bg-red-600">API Status: Offline</h1>
-            }
-            {
-                apiStatus &&
-                <h1 className="bg-green-600">API Status: Online</h1>
-            }
 
             {
                 networkName &&
--- nextjs-alt-wallet-client/components/DashboardApi.js
+++ nextjs-alt-wallet-client/components/DashboardApi.js
@@ -1,12 +1,53 @@
-import React from 'react'
 import Dashboard from './Dashboard';
+import React, { useState, useEffect } from 'react';
 
 const DashboardApi = () => {
+
+    const [apiStatus, setApiStatus] = useState(null);
+
+
+    const testConnexionToApi = () => {
+        return new Promise((resolve, reject) => {
+            try {
+                var url = "http://localhost:7546/"
+                var request = new XMLHttpRequest();
+                request.open('GET', url, true);
+                request.timeout = 5000;
+                request.responseType = 'text';
+                request.send();
+                fetch(url).then(function (response) {
+                    response.text().then(function (text) {
+                        setApiStatus(true);
+                        resolve(text);
+                    });
+                });
+                request.ontimeout = function () {
+                    setApiStatus(null);
+                }
+            } catch (err) {
+                throw new Error(err);                    //console.log(err);
+                setApiStatus(null);
+            }
+        })
+    }
+
+    useEffect(() => {
+        testConnexionToApi();
+        setInterval(testConnexionToApi, 1000);
+    }, [apiStatus]);
+
     return (
         <div className="flex flex-col justify-center text-center">
             <div class="shadow-lg px-4 py-6 bg-gray-100 dark:bg-gray-800 relative m-4">
                 <h1 className="text-4xl">API Dashboard</h1>
-                <Dashboard />
+                {
+                    !apiStatus &&
+                    <h1 className="bg-red-600">API Status: Offline</h1>
+                }
+                {
+                    apiStatus &&
+                    <h1 className="bg-green-600">API Status: Online</h1>
+                }
             </div>
         </div>
     )
