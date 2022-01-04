da5355f6915035bfe73ee985d74b3bc08a851ee1
Cnsbmkpsr <44788831+Cnsbmkpsr@users.noreply.github.com>
2022-01-04 18:07:56 +0100

    Update dashboard

--- nextjs-alt-wallet-client/components/DashboardApi.js
+++ nextjs-alt-wallet-client/components/DashboardApi.js
@@ -1,5 +1,7 @@
 import Dashboard from './Dashboard';
 import React, { useState, useEffect } from 'react';
+import { utils } from 'ethers';
+import { ethers } from 'ethers';
 
 const DashboardApi = ({ props }) => {
 
@@ -7,6 +9,11 @@ const DashboardApi = ({ props }) => {
     const apiUrl = "http://localhost:7546/"
     const apiConnexionTimeout = 10;
     const [transactionsHistory, setTransactionsHistory] = useState(null);
+
+    const [networkName, setNetworkName] = useState();
+    const [signerAddress, setSignerAddress] = useState();
+    const [signerBalance, setSignerBalance] = useState();
+    const [signerWalletTransactionCount, setSignerWalletTransactionCount] = useState();
 
     const apiRequestBuilder = async (ressourcePath, type, params, responseType) => {
         return new Promise((resolve, reject) => {
@@ -48,9 +55,33 @@ const DashboardApi = ({ props }) => {
         }
     }
 
-    const getTransactionHistory = async (props) => {
+    const getWalletInformation = async () => {
         try {
-            let params = "publicKey=" + "0x86B8582BFA4deE84802e9FB6609BBAf065209E3A"
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            let networkName = (await provider.getNetwork()).name;
+            setNetworkName(networkName);
+
+            const signer = provider.getSigner();
+            let signerAddress = (await signer.getAddress());
+            setSignerAddress(signerAddress);
+            getTransactionHistory(signerAddress);
+
+            let signerBalance = (await ethers.utils.formatEther(await signer.getBalance()));
+            setSignerBalance(signerBalance);
+
+            let signerWalletTransactionCount = await signer.getTransactionCount();
+            setSignerWalletTransactionCount(signerWalletTransactionCount);
+
+            await window.ethereum.send("eth_requestAccounts");
+
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    const getTransactionHistory = async (walletAddress) => {
+        try {
+            let params = "publicKey=" + walletAddress
             const apiResponse = await apiRequestBuilder("wallet/transactionsHistory", "GET", params, "json");
             setTransactionsHistory(apiResponse);
             console.log(apiResponse[0])
@@ -62,9 +93,10 @@ const DashboardApi = ({ props }) => {
 
     useEffect(() => {
         try {
+            getWalletInformation();
             testConnexionToApi();
             setInterval(testConnexionToApi, 1000);
-            getTransactionHistory(props);
+
         } catch (err) {
             console.log(err);
         }
@@ -85,16 +117,89 @@ const DashboardApi = ({ props }) => {
                 {
                     transactionsHistory ?
                         <div>
-                            <h1>Transactions: </h1>
-                            {transactionsHistory.map((transaction) =>
-                                <div>
-                                    <p>Transaction number: {transaction.nonce - 1}</p>
-                                    <p>Transaction hash: {transaction.hash}</p>
-                                    <p className="bg-blue-400">Transaction index: {transaction.transactionIndex}</p>
-                                    <p>Transaction done at {transaction.timestamp}</p>
-                                    <p >Transaction done to {transaction.to}</p>
+                            <div class="container mx-auto px-4 sm:px-8">
+                                <div class="py-8 ">
+                                    <div>
+                                        <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
+                                            <table class="min-w-full leading-normal">
+                                                <thead>
+                                                    <tr>
+                                                        <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                                                            Index
+                                                        </th>
+                                                        <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                                                            Hash
+                                                        </th>
+                                                        <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                                                            Gas Price (ETH)
+                                                        </th>
+                                                        <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                                                            value (ETH)
+                                                        </th>
+                                                        <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                                                            timestamp
+                                                        </th>
+                                                        <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                                                            Status
+                                                        </th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    {transactionsHistory.map((transaction) =>
+                                                        <tr>
+                                                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                                <p class="text-gray-900 whitespace-no-wrap">
+                                                                    {transaction.nonce - 1}
+                                                                </p>
+                                                            </td>
+                                                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                                <p class="text-gray-900 whitespace-no-wrap">
+                                                                    {transaction.hash}
+                                                                </p>
+                                                            </td>
+                                                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                                <p class="text-gray-900 whitespace-no-wrap">
+                                                                    {utils.formatEther(transaction.gasPrice)}
+                                                                </p>
+                                                            </td>
+                                                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                                <p class="text-gray-900 whitespace-no-wrap">
+                                                                    {utils.formatEther(transaction.value)}
+                                                                </p>
+                                                            </td>
+                                                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                                <p class="text-gray-900 whitespace-no-wrap">
+                                                                    {new Date(transaction.timestamp * 1000).getDate()}/{new Date(transaction.timestamp * 1000).getMonth()}/{new Date(transaction.timestamp * 1000).getFullYear()}
+                                                                </p>
+                                                            </td>
+                                                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                                <span class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                                                                    <span aria-hidden="true" class="absolute inset-0 bg-green-200 opacity-50 rounded-full">
+                                                                    </span>
+                                                                    <span class="relative">
+                                                                        Done
+                                                                    </span>
+                                                                </span>
+                                                            </td>
+                                                        </tr>
+
+
+                                                        /*
+                                                        <div>
+                                                            <p>Transaction number: {transaction.nonce - 1}</p>
+                                                            <p>Transaction hash: {transaction.hash}</p>
+                                                            <p className="bg-blue-400">Transaction index: {transaction.transactionIndex}</p>
+                                                            <p>Transaction done at {transaction.timestamp}</p>
+                                                            <p >Transaction done to {transaction.to}</p>
+                                                        </div>
+                                                        */
+                                                    )}
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
                                 </div>
-                            )}
+                            </div>
                         </div>
 
                         :
