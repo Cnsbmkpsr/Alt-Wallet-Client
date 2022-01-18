0968375c16aec587d3ae13bc1b3d305328b02a9c
Cnsbmkpsr <44788831+Cnsbmkpsr@users.noreply.github.com>
2022-01-18 15:49:18 +0100

    _app.js fix

--- nextjs-alt-wallet-client/pages/_app.js
+++ nextjs-alt-wallet-client/pages/_app.js
@@ -3,6 +3,7 @@ import "../styles/global.css";
 import Router from "next/router";
 
 import ProgressBar from "@badrap/bar-of-progress";
+import PropTypes from 'prop-types';
 
 const progress = new ProgressBar({
   size: 4,
@@ -17,6 +18,11 @@ Router.events.on('routeChangeError', progress.finish);
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.required,
+  pageProps: PropTypes.required
 }
 
 export default MyApp
