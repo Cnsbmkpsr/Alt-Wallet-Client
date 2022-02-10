7abae26c5c4bee333a14c0759263659e6182c376
Cnsbmkpsr <44788831+Cnsbmkpsr@users.noreply.github.com>
2022-02-10 17:00:14 +0100

    Upgrade UI Dashboard History

--- nextjs-alt-wallet-client/components/DashboardApi.js
+++ nextjs-alt-wallet-client/components/DashboardApi.js
@@ -124,27 +124,29 @@ const DashboardApi = ({ walletAddress }) => {
 
     return (
         <div>
-            <div>
-
-            </div>
             <div className="shadow-lg px-4 py-6 bg-gray-100 dark:bg-gray-800 relative m-4">
+                <h1 className="text-4xl text-center">Transaction history</h1>
+
                 {
                     hasError &&
                     <ErrorMessage message={hasError} />
                 }
                 {
                     apiStatus &&
-                    <div className="bg-green-200 border-green-600 text-green-600 border-l-4 p-4 max-w" role="alert">
-                        <p className="font-bold">
-                            Success
-                        </p>
-                        <p className="max-w-sm">
-                            API Status: Online
-                        </p>
+                    <div>
+                        <div className="bg-green-200 border-green-600 text-green-600 border-l-4 p-4 max-w mt-4" role="alert">
+                            <p className="font-bold">
+                                Success
+                            </p>
+                            <p className="max-w-sm">
+                                API Status: Online
+                            </p>
+                        </div>
+
                     </div>
+
                 }
                 <div className="flex flex-col justify-center text-center">
-                    <h1 className="text-4xl">Transaction history</h1>
 
                     {
                         transactionsHistory ?
@@ -235,7 +237,15 @@ const DashboardApi = ({ walletAddress }) => {
                             :
 
                             <div>
-                                <h3>Recovery of your transaction history, please wait...</h3>
+                                <div className="rounded-md flex items-center jusitfy-between px-5 py-4 mt-2 border border-sky-500 text-sky-500">
+                                    <div className="w-full flex items-center">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className=" w-6 h-6 mr-2" viewBox="0 0 1792 1792">
+                                            <path d="M1024 1375v-190q0-14-9.5-23.5t-22.5-9.5h-192q-13 0-22.5 9.5t-9.5 23.5v190q0 14 9.5 23.5t22.5 9.5h192q13 0 22.5-9.5t9.5-23.5zm-2-374l18-459q0-12-10-19-13-11-24-11h-220q-11 0-24 11-10 7-10 21l17 457q0 10 10 16.5t24 6.5h185q14 0 23.5-6.5t10.5-16.5zm-14-934l768 1408q35 63-2 126-17 29-46.5 46t-63.5 17h-1536q-34 0-63.5-17t-46.5-46q-37-63-2-126l768-1408q17-31 47-49t65-18 65 18 47 49z">
+                                            </path>
+                                        </svg>
+                                        <p>We use public APIs to collect your information. If your history does not appear in a few seconds, refresh the page</p>
+                                    </div>
+                                </div>
                             </div>
                     }
                 </div>
